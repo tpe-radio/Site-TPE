c2a949284348cd9a26a60b1ca80a7b15d6e34860
Arthur Pesah <arthur.pesah@gmail.com>
2011-12-22 18:11:55 +0100

    Ajout des sessions contact et bibliographie

---
+++ contact.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+                <script type="text/javascript" src="scripts/jquery-1.7.1.min.js"></script>
+                <link rel="icon" type="image/png" href="images/favicon.png" />
+                
+		<title>Bibliographie</title>
+                
+		<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		
+		<!--[if lte IE 7]>
+        	<link rel="stylesheet" href="style_ie.css" />
+        	<![endif]-->
+		
+	</head>
+            
+	<body>
+                
+		<?php include("includes/header/header.php"); ?>
+		<?php include("includes/menus/nav_onglets.php"); ?>  
+                
+		<div id="corps">
+		
+			<?php include("includes/menus/nav_gauche.php"); ?>           
+                        
+			<section id="contenu">
+				<h1>Contact</h1>
+			</section>
+		
+		</div>
+                
+                <?php include("includes/footer/footer.php"); ?>	
+                
+	</body>
+</html>
