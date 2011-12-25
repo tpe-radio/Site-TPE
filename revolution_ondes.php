5b517c967d3239cf928b55da263dc340d5a748ef
Arthur Pesah <arthur.pesah@gmail.com>
2011-12-25 22:49:54 +0100

    Rédaction de la sous-partie

--- revolution_ondes.php
+++ revolution_ondes.php
@@ -28,7 +28,88 @@
 		<?php include("includes/menus/nav_gauche.php"); ?>           
                         
 		<section id="contenu">
-			<h1>La Révolution des ondes</h1>
+			<h1>La radio comme moyen de télécommunication</h1>
+			
+			<h2>La Révolution des ondes</h2>
+			
+				<p class="problematique">Quelles sont les différentes découvertes et expériences qui ont ménées à l'invention de la radio à la fin du XIXe siècle ?</p>
+				
+				<h3>La radio, une nécessité historique</h3>
+				
+				<p class="paragraphe">
+					De tout temps, les hommes ont exprimé un besoin de communiquer.
+					Ils utilisèrent d'abord les gestes, la parole et l'écriture.
+					Pour pouvoir aller plus loin dans les communications, ils élaborèrent divers systèmes au cours du temps : les messagers, le courrier postale, les pigeons voyageurs, etc.
+					Ces systèmes étaient souvent trop lents, ou pas assez fiables. 
+					À la fin du XVIIIe siècle, alors que la France traverse un des moments les plus importants de son histoire, un besoin de faire passer rapidement l'information à l'intérieur du pays se fait ressentir.
+					C'est ainsi qu'est élaboré par l'inventeur français Claude Chappe le <strong>télégraphe optique</strong>, ou sémaphore. 
+					Il consiste en une tour très haute, avec deux leviers au sommet, qui, bien dirigés, permettent d'écrire des symboles et de faire ainsi passer des messages à d'autres tours du même type. 
+					Ce système sera utilisé jusqu'au milieu du siècle suivant, où il sera en partie remplacé par le <strong>télégraphe électrique</strong>. 
+					Ce moyen de télécommunication, élaboré grâce aux découvertes en électricité de l'époque, permet une communication beaucoup plus rapide, supprime les contraintes dues aux conditions atmosphériques (le sémaphore ne pouvant être utilisé pendant des brouillards ou de fortes pluies), et permet une diffusion plus ciblée de l'information. 
+					La frontière de l'océan est enfin supprimée, et les communication se font désormais quasi-instantanément à travers le monde. 
+					L'invention du téléphone par Bell en 1877, permet enfin de transmettre la voix sur de longues distances. 
+					Toutefois, malgré une grande amélioration des moyens de communication, la contrainte du fil est toujours présente. Les cables coûtent cher, peuvent être coupés facilement, et, surtout, ne peuvent pas être réliés avec des objets mobiles comme des bâteaux ou des voitures. 
+					C'est ainsi qu'apparaît à la fois accidentellement et de façon nécessaire, à la fin des années 1880, <strong>la télégraphie sans fil</strong>, ou <strong>TSF</strong>, qui deviendra plus tard la radio.
+				</p>
+				
+				<h3>La révolution de l'électromagnétisme</h3>
+				
+				<p class="paragraphe">
+					L'invention de la TSF a été rendue possible en partie grâce aux nombreuses découvertes et experiences physiques qui se sont opérées durant le XIXe siècle.
+					Une nouvelle branche de l'électricité apparaît en effet au début du siècle et prend une  importance particulière dans la communauté scientifique : l'électromagnétisme, c'est-à-dire l'étude des liaisons entre l'électricité et le magnétisme. 
+					On doit l'apparition de cette science à un physicien danois sous le nom d'<strong>Oersted</strong>. 
+					Il découvre lors d'une expérience devant ses élèves en avril 1820 qu'un fil transportant du courant est capable de faire varier les aiguilles d'une boussole. 
+					Les physiciens français <strong>Ampère</strong> et <strong>Arago</strong> suivent de près ses expériences. 
+					Ampère donne une première explication à ce  phénomène, par la création d'un champ magnétique autour d'un courant électrique, et fournit les premiers fondements mathématiques de cette nouvelle science. 
+					Arago découvre qu'en mettant un fil de courant en présence de fer, celui-ci se comporte comme un aimant : il invente ainsi, avec l'aide d'Ampère, l'électroaimant. 
+					Quelques année plus tard, en 1831, <strong>Faraday</strong>, physicien et chimiste britannique, découvre qu'un aimant peut agir sur un circuit électrique, et créer du courant. 
+					Il forge ainsi en 1831 la théorie de l'induction électromagnétique, qui consiste en la transformation d'énergie mécanique en énergie électrique. 
+					Un des apports les plus importants dans la théorie de l'électromagnétisme reste celui de <strong>James Clerk Maxwell</strong>, physicien et mathématicien écossais. 
+					Il parvient en effet à unifier sous la forme de vingt équations les relations entre l'électricité et le magnétisme, et prouve en 1864 l'existence des ondes électromagnétiques, ouvrant ainsi la voie à l'émission d'ondes radio. 
+					Il ne se livrera toutefois à aucune expérience.
+				</p>
+				
+				<h3>La TSF, une invention progressive aux multiples auteurs</h3>
+				
+				<p class="paragraphe">
+					La première transmission d'ondes électromagnétiques est due au physicien allemand Heinrich Hertz. 
+					Dans une volonté de vérifier expérimentalement les prédictions de Maxwell, notamment l'existence d'ondes électriques, il réalise en 1888 un émetteur, constitué d'un éclateur à étincelle alimenté par une bobine, et un détecteur, consistant en une simple boucle de fil de cuivre ouverte, dont les extrêmités sont très proches. 
+					Lorsque l'éclateur fonctionne, il constate qu'une toute petite étincelle apparaît entre les deux extrémités du détecteur, situé à quelques mètres. 
+					L'existence de ce que l'on appellera plus tard les « ondes hertziennes » est démontrée. 
+					Toutefois, ce concept n'a, en l'état, encore aucune application pratique possible. 
+					Deux ans plus tard, le français Edouard Branly, lors de ses études sur la conductibilité, constate qu'un tube de verre contenant de la limaille de fer devient conducteur en présence d'ondes électromagnétiques. 
+					Il s'agit du premier récepteur réellement utilisable, bien qu'il faille encore tapoter dessus entre chaque réceptions pour lui enlever sa conductibilité. 
+					Ce problème sera réglé par le physicien anglais Sir Oliver Lodge en 1894 qui, dans ses amélioration du récepteur, parvient à enlever la conductibilité de la limaille automatiquement. 
+					En 1895, lors de ses études sur la détection des orages, l'ingénieur de la Marine Impériale Russe Alexandre Popov a l'idée de raccorder le fil d'un paratonnerre, relié à la terre, à un récepteur de Branly-Lodge. 
+					Il invente ainsi l'antenne-terre, qui permet d'accroître fortement les distances. 
+					Malgré un grand intérêt de tous ces physiciens dans l'étude des ondes et de leur émission-recéption, aucun n'imaginait réellement utiliser ce système pour transmettre de l'information. 
+					Les distances paraissaient trop grandes à franchir, et beaucoup ne voyaient dans ce nouveau système qu'un intérêt pédagogique.
+				</p>
+				
+				<p class="paragraphe">
+					De son côté, depuis les années 1890, le jeune étudiant Italien <strong>Guglielmo Marconi</strong> suit avec beaucoup d'intérêt les expériences de ses contemporains. 
+					Il assiste à l'université de Bologne à des cours et expériences sur les ondes, qu'il voit déjà comme un moyen pour transmettre de l'information. 
+					Il a 21 ans lorsqu'il transmet des signaux morses pour la première fois, à une quarantaine de mètres de distance, en utilisant l'émetteur de Hertz, le récepteur de Branly-Lodge, et l'antenne de Popov. 
+					La TSF est née. 
+					Il se lance alors sur le projet d'augmenter les distances, et en fera une obsession technique. 
+					Il atteint les cent mètre à la fin de l'année 1895. 
+					Entrevoyant l'immense potentialité de ce qu'il venait d'inventer, il part en Angleterre en 1896, où il dépose un brevet. 
+					Après avoir atteint les cinq, dix, quinze kilomètres, il parvient à effectuer sa première liaison à travers la Manche (46 km) en 1899, et atteint une distance de plus de 3000 km à travers l'Atlantique deux ans plus tard. 
+					La TSF commence à se faire connaître partout dans le monde. 
+					En France, c'est l'ingénieur <strong>Eugène Ducretet</strong> qui établira les premières liaisons, d'abord du Pantéon jusqu'à son laboratoire (400 m), puis jusqu'à la Tour Eiffel (4 km). 
+					La voix est transmise sur les ondes pour la première fois en 1900 grâce au Canadien Reginald Fessenden. 
+					Toutefois, la qualité est encore assez mauvaise, ce qui rend la TSF moins efficace que le télégraphe électrique. 
+					L'ingénieur <strong>Nicolas Tesla</strong> améliore la qualité de reception en réglant la longueur d'onde du circuit du détecteur sur celle de l'émetteur. 
+					En 1904, <strong>John Fleming</strong> dépose le brevet d'une lampe à deux électrodes détectant les ondes électromagnétiques. 
+					Lee de Forest l'améliore en y ajoutant une troisième électrode, et invente ainsi la triode, qui permet d'amplifier l'intensité du signal. 
+					Le militaire français <strong>Gustave Ferrié</strong>, général dans l'armée de terre et dans la Marine Nationale, invente en 1900 le détecteur électrolytique, plus performant que le récepteur de Lodge. 
+					On voit donc bien que la TSF n'est pas le fruit d'un seul homme, mais bien une combinaison d'avancées scientifiques et techniques, dont on entrevoit très vite les possibilités en terme de télécommunication.
+				</p>
+				
+				<p class="paragraphe">							
+					Ainsi, la radio est née de la rencontre entre une nécessité historique, le besoin d'un nouveau moyen de communiquer, sans utiliser de fil, et une révolution d'un point de vue physique : la découverte des ondes électromagnétiques. Grâce à de nombreux scientifiques, mais également à des hommes qui en ont vu les potentialités pratiques, la TSF apparaît au milieu des années 1890, et, après plusieurs améliorations, est prête à être utilisée au début XXe siècle. Les militaires et les marins s'intéressent de prêt à cette nouvelle invention, d'autant plus que la sensation d'une guerre proche monte de jour en jour.
+				</p>
+			<h2>
 		</section>
                 
                 <?php include("includes/footer/footer.php"); ?>	
