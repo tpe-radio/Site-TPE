4676be8ca601e64fed270bf96fd6678f73cacb97
Arthur Pesah <arthur.pesah@gmail.com>
2011-12-25 20:45:02 +0100

    Ajout des pages correspondantes aux parties

---
+++ revolution_ondes.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+                <script type="text/javascript" src="scripts/jquery-1.7.1.min.js"></script>
+                <link rel="icon" type="image/png" href="images/favicon.png" />
+                
+		<title>L'évolution dans l'utilisation de la radio à travers les deux guerres mondiales − TPE 2011-2012 par Arthur et Elyes</title>
+                
+		<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		
+		<!--[if lte IE 7]>
+        	<link rel="stylesheet" href="style_ie.css" />
+        	<![endif]-->
+		
+	</head>
+            
+	<body>
+                
+		<?php include("includes/header/header.php"); ?>
+		<?php include("includes/menus/nav_onglets.php"); ?>  
+                
+		<div id="corps">
+		
+		<?php include("includes/menus/nav_gauche.php"); ?>           
+                        
+		<section id="contenu">
+			<h1>La Révolution des ondes</h1>
+		</section>
+                
+                <?php include("includes/footer/footer.php"); ?>	
+                
+	</body>
+</html>
